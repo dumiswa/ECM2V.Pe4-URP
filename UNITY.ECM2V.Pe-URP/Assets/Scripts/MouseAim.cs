ddd378bfd9909d999fa52c96304e9ec15397679c
dumiswa <dumitrudumi605@yahoo.com>
2023-09-19 01:31:35 +0200

    19/09/2023v2

--- UNITY.ECM2V.Pe-URP/Assets/Scripts/MouseAim.cs
+++ UNITY.ECM2V.Pe-URP/Assets/Scripts/MouseAim.cs
@@ -1,23 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseAim : MonoBehaviour
 {
-
     public float mouseSensitivity = 100f;
+    public float walkingBobbingSpeed = 14f;
+    public float bobbingAmount = 0.05f;
 
     float xRotation = 0f;
+    float timer = 0;
+
+    // Store the initial local position of the camera
+    Vector3 initialLocalPosition;
 
     public Transform playerBody;
+
+    // Modify this to indicate if the player is moving
+    bool isMoving = false;
 
     void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
+
+        // Store the initial local position
+        initialLocalPosition = transform.localPosition;
     }
 
-
     void Update()
+    {
+        HandleMouseAim();
+        HandleBobbing();
+    }
+
+    void HandleMouseAim()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
@@ -27,5 +41,24 @@ public class MouseAim : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, transform.localRotation.z);
         playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    void HandleBobbing()
+    {
+        // Modify this to indicate if the player is moving
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f || Mathf.Abs(Input.GetAxis("Vertical")) > 0.1f)
+        {
+            isMoving = true;
+            timer += Time.deltaTime * walkingBobbingSpeed;
+        }
+        else
+        {
+            isMoving = false;
+            timer = 0;
+        }
+
+        // Calculate the bobbing effect
+        float bobbingOffset = isMoving ? Mathf.Sin(timer) * bobbingAmount : 0f;
+        transform.localPosition = new Vector3(initialLocalPosition.x, initialLocalPosition.y + bobbingOffset, initialLocalPosition.z);
     }
 }
