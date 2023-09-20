eedc49952412e5ee1541aa6c7c3c6317381a3b65
dumiswa <dumitrudumi605@yahoo.com>
2023-09-20 14:40:34 +0200

    20/09/2023

--- UNITY.ECM2V.Pe-URP/Assets/Scripts/walkingAudio.cs
+++ UNITY.ECM2V.Pe-URP/Assets/Scripts/walkingAudio.cs
@@ -9,18 +9,16 @@ public class walkingAudio : MonoBehaviour
     private bool isWalking = false;
 
     void Update()
-    {
-        // Check if any movement key is pressed
+    {     
         bool isMoving = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
 
-        // If any movement key is pressed and the audio is not playing, start playing it
         if (isMoving && !isWalking)
         {
             grassWalk.Play();
             grassWalk.loop = true;
             isWalking = true;
         }
-        // If no movement keys are pressed and the audio is playing, stop it
+
         else if (!isMoving && isWalking)
         {
             grassWalk.Stop();
