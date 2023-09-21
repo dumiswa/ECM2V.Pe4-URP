c46dffe14698c30c907ad88aa4b5141179610614
dumiswa <dumitrudumi605@yahoo.com>
2023-09-21 19:45:17 +0200

    sd

--- UNITY.ECM2V.Pe-URP/Assets/Scripts/MainMenu.cs
+++ UNITY.ECM2V.Pe-URP/Assets/Scripts/MainMenu.cs
@@ -13,7 +13,7 @@ public class MainMenu : MonoBehaviour
     }
     public void Play()
     {
-        SceneManager.LoadScene("Prototype");
+        SceneManager.LoadScene("MainScene");
     }
 
     public void Options()
