599dcd46f27cab03acef752508fa5e63b10d3030
dumiswa <dumitrudumi605@yahoo.com>
2023-09-22 06:19:51 +0200

    6:19 AM

--- UNITY.ECM2V.Pe-URP/Assets/Scripts/Options.cs
+++ UNITY.ECM2V.Pe-URP/Assets/Scripts/Options.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class Options : MonoBehaviour
 {
+
+   void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void Back()
     {
         SceneManager.LoadScene("MainMenu");
